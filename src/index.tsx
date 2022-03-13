1d9365f17917e6469dfeb9da648b01c786bf6a99
ckieverentineas <ckieverentineas@yandex.ru>
2022-03-13 14:09:18 +1000

    created design main page

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App/App';
 import Buttons from './components/button/button';
 import Headers from './components/header/header';
 import "./index.css"
@@ -8,9 +7,20 @@ import "./index.css"
 
 ReactDOM.render(
   <React.StrictMode>
-    <Headers />
+    <div>
+      <div>
+        <Headers />
+      </div>
+      <div>
+        <img className={'next'}
+          src="/img/main-page.jpg"
+          alt="button-next"
+        />
+      </div>
+      <h1>Let’s find your perfect pair!</h1>
+      <p>Take the quiz to easily discover your perfect fit from thousands of styles</p>
     <Buttons name="Start Now"/>
-    <App />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
