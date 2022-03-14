eab0f6060f675df6898f49c3b0e35bac54a785ae
ckieverentineas <ckieverentineas@yandex.ru>
2022-03-14 18:32:22 +1000

    changed photo jpg on png in main-page and header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,9 +3,9 @@ import { Logo, Next, Wrap } from './Header.components'
 export function Header() {
   return (
     <Wrap>
-      <Logo src="/img/logo.jpg" alt="Optimax Dev" />
+      <Logo src="/img/logo.png" alt="Optimax Dev" />
 
-      <Next src="/img/button-next.jpg" alt="button-next" />
+      <Next src="/img/button-next.png" alt="button-next" />
     </Wrap>
   )
 }
