64006d90601fc2f04bec2ef57fc7f1f51476b3ce
ckieverentineas <ckieverentineas@yandex.ru>
2022-03-14 19:20:31 +1000

    edit button styles

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,12 +1,23 @@
 import styled from '@emotion/styled/macro'
 
 export const Button = styled.button`
-  border-radius: 10px;
-  color: white;
-  padding: 10px;
-  width: auto;
+  padding: 13px 48px 14px 48px;
   font-weight: bolder;
-  background: linear-gradient(45deg, teal, #42aaff);
+  border: none;
+
+  font-family: 'Roboto';
+  font-style: normal;
+  font-weight: 700;
+  font-size: 18px;
+  line-height: 21px;
+
+  text-align: center;
+
+  color: #FFFFFF;
+
+  background: linear-gradient(270deg, #45C7FA 0%, #2196F3 100%);
+  box-shadow: 0px 0px 4px 1px rgba(0, 0, 0, 0.03);
+  border-radius: 24px;
 
   &:hover {
     background: linear-gradient(45deg, #42aaff, teal);
