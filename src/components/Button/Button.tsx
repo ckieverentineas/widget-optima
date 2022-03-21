1e77c67005f4a63aacfc1f1bd0c4fb4af842a9ae
ckieverentineas <ckieverentineas@yandex.ru>
2022-03-21 22:42:10 +1000

    correct stules button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,28 +1,27 @@
 import styled from '@emotion/styled/macro'
 
 export const Button = styled.button`
+  cursor: pointer;
   padding: 13px 48px 14px 48px;
   font-weight: bolder;
   border: none;
 
-  font-family: 'Roboto';
   font-style: normal;
-  font-weight: 700;
   font-size: 18px;
   line-height: 21px;
 
   text-align: center;
 
-  color: #FFFFFF;
+  color: #ffffff;
 
-  background: linear-gradient(270deg, #45C7FA 0%, #2196F3 100%);
+  background: linear-gradient(270deg, #45c7fa 0%, #2196f3 100%);
   box-shadow: 0px 0px 4px 1px rgba(0, 0, 0, 0.03);
   border-radius: 24px;
 
   &:hover {
-    background: linear-gradient(45deg, #42aaff, teal);
+    filter: brightness(1.1);
   }
   &:disabled {
-    background: grey;
+    background: #dedede;
   }
 `
