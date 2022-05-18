9dbcabf4b2b0d43e3c8c9f3a78e3628cf2e371da
ckieverentineas <36624728+ckieverentineas@users.noreply.github.com>
2022-05-18 23:14:06 +1000

    edit button styles (#1)
    * edit button styles
    
    * correct stules button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,17 +1,27 @@
 import styled from '@emotion/styled/macro'
 
 export const Button = styled.button`
-  border-radius: 10px;
-  color: white;
-  padding: 10px;
-  width: auto;
+  cursor: pointer;
+  padding: 13px 48px 14px 48px;
   font-weight: bolder;
-  background: linear-gradient(45deg, teal, #42aaff);
+  border: none;
+
+  font-style: normal;
+  font-size: 18px;
+  line-height: 21px;
+
+  text-align: center;
+
+  color: #ffffff;
+
+  background: linear-gradient(270deg, #45c7fa 0%, #2196f3 100%);
+  box-shadow: 0px 0px 4px 1px rgba(0, 0, 0, 0.03);
+  border-radius: 24px;
 
   &:hover {
-    background: linear-gradient(45deg, #42aaff, teal);
+    filter: brightness(1.1);
   }
   &:disabled {
-    background: grey;
+    background: #dedede;
   }
 `
